290387d1bddda9add4755f47c71d18a726b50abe
Samira <grossisamira@hotmail.com>
2024-08-26 19:25:20 -0300

    chore: adds packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      components: path.resolve(__dirname, "./src/components"),
+      pages: path.resolve(__dirname, "./src/pages"),
+      models: path.resolve(__dirname, "./src/models"),
+      contexts: path.resolve(__dirname, "./src/contexts"),
+      services: path.resolve(__dirname, "./src/services"),
+    },
+  },
+});
