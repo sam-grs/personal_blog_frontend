f59d260f0df674a095da209fc040818436d2ddc4
Samira <grossisamira@hotmail.com>
2024-08-26 20:12:43 -0300

    feat: adds AuthContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Footer } from "./components/footer/Footer";
-import { Header } from "./components/header/Header";
-import { Home } from "./pages/home/Home";
-import { Register } from "./pages/register/Register";
+import { Header, Footer } from "components";
+import { Home, Login, Register } from "pages";
+import { AuthProvider } from "contexts/AuthContext";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
