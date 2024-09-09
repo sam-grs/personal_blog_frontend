7bbee72201be543fd192353207b829f6d9820cd4
Samira <grossisamira@hotmail.com>
2024-09-08 22:47:46 -0300

    feat: adds styles to Footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,22 +1,19 @@
 import { LinkedinLogo, GithubLogo } from "@phosphor-icons/react";
 
 export function Footer() {
-  const data = new Date().getFullYear();
+  const date = new Date().getFullYear();
 
   return (
-    <div className="flex justify-center bg-indigo-700 text-white">
-      <div className="container flex flex-col items-center py-4 gap-2">
-        <h4>Blog Pessoal {data}</h4>
-
-        <div className="flex gap-2">
-          <a href="https://github.com/sam-grs">
-            <GithubLogo size={30} />
-          </a>
-          <a href="https://www.linkedin.com/in/samira-grossi/">
-            <LinkedinLogo size={30} />
-          </a>
-        </div>
+    <div className="bg-dark_purple text-pink flex flex-col items-center justify-center gap-2 h-[100px]">
+      <div className="flex">
+        <a href="https://github.com/sam-grs">
+          <GithubLogo size={30} />
+        </a>
+        <a href="https://www.linkedin.com/in/samira-grossi/">
+          <LinkedinLogo size={30} />
+        </a>
       </div>
+      <h4>Galactka {date}</h4>
     </div>
   );
 }
